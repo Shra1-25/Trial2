0bd08f3da55301dae1f5309c1c4b07dca96c6bd1
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-06-02 04:29:00 +0530

    Update tp3.cc

--- tp3.cc
+++ tp3.cc
@@ -53,7 +53,7 @@ int main(int argc, char* argv[]) {
     Tensor x(DT_FLOAT, TensorShape({100, 32}));
     Tensor y(DT_FLOAT, TensorShape({100, 8}));
     std::copy_n(X_vec.begin(), X_vec.size(), x.flat<float>().data());
-    auto _XTensor = x.matrix<float>();
+    //auto _XTensor = x.matrix<float>();
     auto _YTensor = y.matrix<float>();
     
     //_XTensor.setRandom();
