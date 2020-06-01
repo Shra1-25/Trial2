44866b1d5c3a7e948c35557eafe021f4cf009bfd
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-06-02 03:46:28 +0530

    Add files via upload

--- tp2.cc
+++ tp2.cc
@@ -1,51 +1,26 @@
-#include "tensorflow/core/public/session.h"
-#include "tensorflow/core/graph/default_device.h"
-using namespace tensorflow;
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
 using namespace std;
 
-int main(int argc, char* argv[]) {
+int main(){
 
-    std::cout<<"Hello"<<endl;
-	std::string graph_definition = "/home/cmsusr/CMSSW_11_0_1/src/Trial2/graph3.pb";
-    Session* session;
-    GraphDef graph_def;
-    SessionOptions opts;
-    std::vector<Tensor> outputs; // Store outputs
-    TF_CHECK_OK(ReadBinaryProto(Env::Default(), graph_definition, &graph_def));
-	//std::cout<<"Done1"<<endl;
-    // Set GPU options
-    //graph::SetDefaultDevice("/gpu:0", &graph_def);
-    //opts.config.mutable_gpu_options()->set_per_process_gpu_memory_fraction(0.5);
-    //opts.config.mutable_gpu_options()->set_allow_growth(true);
-	//std::cout<<"Done2"<<endl;
-    // create a new session
-    TF_CHECK_OK(NewSession(opts, &session));
-	//std::cout<<"Done3"<<endl;
-    // Load graph into session
-    TF_CHECK_OK(session->Create(graph_def));
-	//std::cout<<"Done4"<<endl;
-    // Initialize our variables
-    TF_CHECK_OK(session->Run({}, {}, {"init_all_vars_op"}, nullptr));
+std::ifstream  data("X_data.csv");
+    std::string line;
+    std::vector<std::vector<float> > parsedCsv;
+    int i=0;
+    while(std::getline(data,line))
+    {
+        std::stringstream lineStream(line);
+        std::string cell;
+        std::vector<float> parsedRow;
+        while(std::getline(lineStream,cell,','))
+        {
+            parsedRow.push_back(std::stof(cell));
+        }
 
-    Tensor x(DT_FLOAT, TensorShape({100, 32}));
-    Tensor y(DT_FLOAT, TensorShape({100, 8}));
-    auto _XTensor = x.matrix<float>();
-    auto _YTensor = y.matrix<float>();
-	std::cout<<"Done";
-    _XTensor.setRandom();
-    _YTensor.setRandom();
-	//std::cout<<"Done5"<<endl;
-    for (int i = 0; i < 10; ++i) {
-        
-        TF_CHECK_OK(session->Run({{"x", x}, {"y", y}}, {"cost"}, {}, &outputs)); // Get cost
-        float cost = outputs[0].scalar<float>()(0);
-        std::cout << "Cost: " <<  cost << std::endl;
-        TF_CHECK_OK(session->Run({{"x", x}, {"y", y}}, {}, {"train"}, nullptr)); // Train
-        outputs.clear();
+        parsedCsv.push_back(parsedRow);
+        cout<<++i<<" "<<parsedRow[0]<<" to "<<parsedRow[31]<<endl;
     }
-	//std::cout<<"Done6"<<endl;
-    session->Close();
-    delete session;
-	std::cout<<"Done"<<endl;
-    return 0;
 }
