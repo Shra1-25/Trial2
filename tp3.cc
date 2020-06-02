90123be3e6e57b5917bb10b714fbfcb446f60325
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-06-02 11:48:23 +0530

    Update tp3.cc

--- tp3.cc
+++ tp3.cc
@@ -36,7 +36,6 @@ int main(int argc, char* argv[]) {
     //std::copy_n(X_vec.begin(), X_vec.size(), x.flat<float>().data());
     auto _XTensor = x.matrix<float>();
     auto _YTensor = y.matrix<float>();
-    std::cout<<"Done1"<<endl;
     
     std::ifstream  data("X_data.csv");
     std::string line;
@@ -45,25 +44,43 @@ int main(int argc, char* argv[]) {
     {
         std::stringstream lineStream(line);
         std::string cell;
-        std::vector<float> parsedRow;
+        //std::vector<float> parsedRow;
         int j_idx=0;
         
         while(std::getline(lineStream,cell,','))
         {
             _XTensor(i_idx,j_idx)=std::stof(cell);
-            parsedRow.push_back(std::stof(cell));
+            //parsedRow.push_back(std::stof(cell));
             j_idx++;
         }
 
         //X_vec.push_back(parsedRow);
-        std::cout<<i_idx<<" "<<parsedRow[0]<<" to "<<parsedRow[31]<<" Done "<<endl;
         i_idx++;
     }
-    
+    std::cout<<"Reading X_data done."<<endl;
+    std::ifstream  data("Y_data.csv");
+    std::string line;
+    int i_idx=0;
+    while(std::getline(data,line))
+    {
+        std::stringstream lineStream(line);
+        std::string cell;
+        //std::vector<float> parsedRow;
+        int j_idx=0;
+        
+        while(std::getline(lineStream,cell,','))
+        {
+            _YTensor(i_idx,j_idx)=std::stof(cell);
+            //parsedRow.push_back(std::stof(cell));
+            j_idx++;
+        }
+
+        //X_vec.push_back(parsedRow);
+        i_idx++;
+    }
     //_XTensor.setRandom();
     //std::copy_n(X_vec.begin(), X_vec.size(), _XTensor.flat<float>().data());
-    _YTensor.setRandom();
-    std::cout<<"Done6"<<endl;
+    //_YTensor.setRandom();
     for (int i = 0; i < 10; ++i) {
         
         TF_CHECK_OK(session->Run({{"x", x}, {"y", y}}, {"cost"}, {}, &outputs)); // Get cost
