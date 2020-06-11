47c983ffb8a42118884fd4cda0ab0a2f561a7cec
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-06-12 02:25:39 +0530

    Update tp3.cc

--- tp3.cc
+++ tp3.cc
@@ -10,7 +10,7 @@ using namespace std;
 int main(int argc, char* argv[]) {
     std::cout<<"Hello"<<endl;    
     
-    std::string graph_definition = "output_graph.pb";
+    std::string graph_definition = "tf_graph.pb";
     Session* session;
     GraphDef graph_def;
     SessionOptions opts;
