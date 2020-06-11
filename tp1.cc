c26f1f8b39d6bbf91252c600573c880367b1a044
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-06-12 03:17:23 +0530

    Update tp1.cc

--- tp1.cc
+++ tp1.cc
@@ -1,72 +1,107 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
 #include "tensorflow/core/public/session.h"
-#include "tensorflow/core/platform/env.h"
-
+#include "tensorflow/core/graph/default_device.h"
 using namespace tensorflow;
+using namespace std;
 
-int main() {
-  // Initialize a tensorflow session
-  Session* session;
-  Status status = NewSession(SessionOptions(), &session);
-  if (!status.ok()) {
-    std::cout << status.ToString() << "\n";
-    return 1;
-  }
+int main(int argc, char* argv[]) {
+    std::cout<<"Hello"<<endl;    
+    
+    std::string graph_definition = "tf_graph.pb";
+    Session* session;
+    GraphDef graph_def;
+    SessionOptions opts;
+    std::vector<Tensor> outputs; // Store outputs
+    TF_CHECK_OK(ReadBinaryProto(Env::Default(), graph_definition, &graph_def));
 
-  // Read in the protobuf graph we exported
-  // (The path seems to be relative to the cwd. Keep this in mind
-  // when using `bazel run` since the cwd isn't where you call
-  // `bazel run` but from inside a temp folder.)
-  GraphDef graph_def;
-  status = ReadBinaryProto(Env::Default(), "/home/cmsusr/CMSSW_11_0_1/src/PhysicsTools/TensorFlow/test/graph.pb", &graph_def);
-  if (!status.ok()) {
-    std::cout << status.ToString() << "\n";
-    return 1;
-  }
+    // Set GPU options
+    //graph::SetDefaultDevice("/gpu:0", &graph_def);
+    //opts.config.mutable_gpu_options()->set_per_process_gpu_memory_fraction(0.5);
+    //opts.config.mutable_gpu_options()->set_allow_growth(true);
 
-  // Add the graph to the session
-  status = session->Create(graph_def);
-  if (!status.ok()) {
-    std::cout << status.ToString() << "\n";
-    return 1;
-  }
+    // create a new session
+    TF_CHECK_OK(NewSession(opts, &session));
 
-  // Setup inputs and outputs:
+    // Load graph into session
+    TF_CHECK_OK(session->Create(graph_def));
+    std::cout<<"Done1"<<endl;
+    // Initialize our variables
+    //TF_CHECK_OK(session->Run({}, {}, {"init_all_vars_op"}, nullptr));
+    std::cout<<"Done2"<<endl;
+    Tensor temp(DT_FLOAT, TensorShape({28, 28}));
+    
+    //Tensor x(DT_FLOAT, TensorShape({100, 32}));
+    //Tensor y(DT_FLOAT, TensorShape({100, 8}));
+    //std::copy_n(X_vec.begin(), X_vec.size(), x.flat<float>().data());
+    auto _XTensor = temp.matrix<float>();
+    //auto _YTensor = y.matrix<float>();
+    
+    /*std::ifstream  data("X_data.csv");
+    std::string line;
+    int i_idx=0;
+    while(std::getline(data,line))
+    {
+        std::stringstream lineStream(line);
+        std::string cell;
+        //std::vector<float> parsedRow;
+        int j_idx=0;
+        
+        while(std::getline(lineStream,cell,','))
+        {
+            _XTensor(i_idx,j_idx)=std::stof(cell);
+            //parsedRow.push_back(std::stof(cell));
+            j_idx++;
+        }
 
-  // Our graph doesn't require any inputs, since it specifies default values,
-  // but we'll change an input to demonstrate.
-  Tensor a(DT_FLOAT, TensorShape());
-  a.scalar<float>()() = 3.0;
+        //X_vec.push_back(parsedRow);
+        i_idx++;
+    }
+    std::cout<<"Reading X_data done."<<endl;
+    
+    std::ifstream  data2("Y_data.csv");
+    //std::string line;
+    i_idx=0;
+    while(std::getline(data2,line))
+    {
+        std::stringstream lineStream(line);
+        std::string cell;
+        //std::vector<float> parsedRow;
+        int j_idx=0;
+        
+        while(std::getline(lineStream,cell,','))
+        {
+            _YTensor(i_idx,j_idx)=std::stof(cell);
+            //parsedRow.push_back(std::stof(cell));
+            j_idx++;
+        }
 
-  Tensor b(DT_FLOAT, TensorShape());
-  b.scalar<float>()() = 2.0;
+        //X_vec.push_back(parsedRow);
+        i_idx++;
+    }*/
+    _XTensor.setRandom();
+    Tensor x(DT_FLOAT, TensorShape({1, 28, 28, 1}));
+    if(!x.CopyFrom(tmp, TensorShape({1, 28, 28, 1}))){
+      std::cout<<"Reshape not successfull."<<endl;
+    }
+    //std::copy_n(X_vec.begin(), X_vec.size(), _XTensor.flat<float>().data());
+    //_YTensor.setRandom();
+    for (int i = 0; i < 10; ++i) {
+        std::cout<<"Done3"<<endl;
+        TF_CHECK_OK(session->Run({{"x", x}/*, {"y", y}*/}, {"dense_2_out"}, {}, &outputs)); // Get cost
+        std::cout<<"Done4"<<endl;
+        float cost = outputs[0].matrix<float>()(0,0);
+        std::cout << "Output: " <<  cost << std::endl;
+        //TF_CHECK_OK(session->Run({{"x", x}, {"y", y}}, {}, {"train"}, nullptr)); // Train
+        outputs.clear();
+    }
 
-  std::vector<std::pair<string, tensorflow::Tensor>> inputs = {
-    { "a", a },
-    { "b", b },
-  };
-
-  // The session will initialize the outputs
-  std::vector<tensorflow::Tensor> outputs;
-
-  // Run the session, evaluating our "c" operation from the graph
-  status = session->Run(inputs, {"c"}, {}, &outputs);
-  if (!status.ok()) {
-    std::cout << status.ToString() << "\n";
-    return 1;
-  }
-
-  // Grab the first output (we only evaluated one graph node: "c")
-  // and convert the node to a scalar representation.
-  auto output_c = outputs[0].scalar<float>();
-
-  // (There are similar methods for vectors and matrices here:
-  // https://github.com/tensorflow/tensorflow/blob/master/tensorflow/core/public/tensor.h)
-
-  // Print the results
-  std::cout << outputs[0].DebugString() << "\n"; // Tensor<type: float shape: [] values: 30>
-  std::cout << output_c() << "\n"; // 30
-
-  // Free any resources used by the session
-  session->Close();
-  return 0;
+    session->Close();
+    delete session;
+    //std::cout<<_YTensor(0,0)<<" "<<_YTensor(0,1)<<" "<<_YTensor(0,2)<<" "<<_YTensor(0,3)<<" "<<_YTensor(0,4)<<" "<<_YTensor(0,5)<<" "<<_YTensor(0,6)<<" "<<_YTensor(0,7)<<" "<<_YTensor(0,8)<<" "<<_YTensor(0,9)<<endl;
+    //std::cout<<_YTensor(1,0)<<" "<<_YTensor(1,1)<<" "<<_YTensor(1,2)<<" "<<_YTensor(1,3)<<" "<<_YTensor(1,4)<<" "<<_YTensor(1,5)<<" "<<_YTensor(1,6)<<" "<<_YTensor(1,7)<<" "<<_YTensor(1,8)<<" "<<_YTensor(1,9)<<endl;
+    std::cout<<"All done"<<endl;
+    return 0;
 }
